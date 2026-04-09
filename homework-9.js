cb8c55c03e6b134cb0e54de438e4341a18afb44c
Muhajir <Karniev@yandex.ru>
2026-04-10 00:06:19 +0400

    (homework №9): add footer and modal form

---
+++ homework-9.js
@@ -0,0 +1,60 @@
+/*4. По нажатию на "Подписаться" выводить в консоль лог объект в виде:  {email: 'введенная почта'} */
+
+const emailForm = document.querySelector(".subscribe-form");
+emailForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+  emailForm.reset();
+});
+
+
+/*5, 6 открываем модальное окно */
+
+const modal = document.querySelector(".modal");
+const registrationBtn = document.getElementById("registration-button");
+const closeBtn = document.querySelector(".close-button");
+
+const openForm = registrationBtn.addEventListener('click', ()=> {
+  modal.classList.add("modal-show");
+});
+
+const closeForm = closeBtn.addEventListener('click', ()=> {
+  modal.classList.remove("modal-show");
+});
+
+const passwordInput= document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+const regForm = document.querySelector(".registration-form");
+
+regForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formElement = event.target;
+  const formData = new FormData(formElement);
+  const formValues = Object.fromEntries(formData.entries());
+  if (!regForm.checkValidity()) {
+    alert("Проверьте корректность заполнения полей!");
+    return;
+  };
+  let userPassword = formData.get("password");
+  let confirmPassword = formData.get("confirm-password");
+  if (userPassword === confirmPassword) {
+    modal.classList.remove("modal-show");
+    user = {
+      ...formValues,
+      createdOn: new Date().toLocaleDateString()
+    };
+    console.log(user);
+    regForm.reset();
+  }
+  else {
+    alert ("Регистрация отклонена, введенный пароль не совпадает, повторите ввод пароля");
+    passwordInput.value = "";
+    confirmPasswordInput.value = "";
+  };
+});
+
+
+
